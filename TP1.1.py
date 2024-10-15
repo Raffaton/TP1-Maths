a8274909748c8bd7370ed27b38c555313cd24a17
Raffaton <raphael.souchet.85@gmail.com>
2024-10-15 14:01:16 +0200

    Add files via upload

---
+++ TP1.1.py
@@ -0,0 +1,13 @@
+from cmath import *
+
+def ex1():
+    a1 = input("Pour la fonction z1, que vaut a : ")
+    b1 = input("Puis b (précisez si le nombre est positif ou négatif) : ")
+
+    a2 = input("Pour la fonction z2, que vaut a : ")
+    b2 = input("Puis b (précisez si le nombre est positif ou négatif) : ")
+
+    print("Ainsi, z1 = {} {}j et z2 = {} {}j".format(a1, b1, a2, b2))
+
+
+test = ex1()
